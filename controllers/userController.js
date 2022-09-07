5c303560af414e2bd04dbe0485f047ff1a7b6bde
nazmul162001 <nazmulprime162001@gmail.com>
2022-09-07 20:22:03 +0600

    create api for added new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,6 +30,41 @@ const getAllUser = (req, res) => {
 };
 
 
+// Add New Random User
+const saveRandomUser = (req, res) => {
+  try {
+    const { gender, name, contact, address, photoUrl } = req.body;
+    if (!gender || !name || !contact || !address || !photoUrl) {
+      res.status(404).json({
+        message:
+          "Properties missing! please Must  include { gender , name ,contact ,photoUrl }",
+      });
+      return;
+    }
+    const jsonData = fs.readFileSync("config.json");
+    const data = JSON.parse(jsonData);
+    const newUser = {
+      id: data.length + 1,
+      gender,
+      name,
+      contact,
+      address,
+      photoUrl,
+    };
+    fs.readFile("config.json", function (err, data) {
+      const json = JSON.parse(data);
+      json.push(newUser);
+      fs.writeFile("config.json", JSON.stringify(json), function (err) {
+        if (err) throw err;
+      });
+    });
+    res.status(200).json({ message: " Successfully Saved Random User " });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+
 // Export All Controllers  Function
 module.exports = {
   getRandomUser,
